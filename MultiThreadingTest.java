2ff631a2cc730d731fb0159c9a62460a1463f2e8
reachram <38026793+reachram@users.noreply.github.com>
2018-10-04 16:36:57 -0500

    Creating a sample multi threading test class

---
+++ MultiThreadingTest.java
@@ -0,0 +1,20 @@
+package com.corejava;
+
+public class MultiThreadingTest {
+
+		private int count;
+
+		public int getCount() {
+			 return this.count;
+		}
+
+		public void setCount(int value) {
+		 this.count = value;
+		}
+
+		public static void main (String args[]) {
+				MultiThreadingTest test = new MultiThreadingTest();
+				test.setCount(77);
+				system.out.println("This is Count value" + test.getCount());
+		}
+}
